14035c49a7ef870548494b4c5360a9d4c39ba18b
David Flores <dcflores@live.com>
2023-03-26 20:59:02 -0700

    Added current day and time

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+//  var now  = dayjs()
+//  console.log(now);
+
 $(function () {
   //Save funtionality
   $(".saveBtn").on("click", function () {
@@ -17,7 +20,7 @@ $(function () {
     // get hour from div
     // get current hour from day js
     // compare the value down below
-    //if() {
+   //if() {
     
    // }else if () {
 
@@ -33,20 +36,14 @@ $(function () {
   });
 
 
-
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
+  // --TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
   // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
+  // past, present, and future classes?
+  // --How can Day.js be used to get the
   // current hour in 24-hour time?
   // loop over each time block
+
   const today = dayjs();
   const currentHour = parseInt(today.format("H"));
   console.log(currentHour);
@@ -66,4 +63,15 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  
+ //current day----------------------
+ 
+
 });
+
+const now = dayjs();
+const currentDay = now.format('dddd');
+const currentTime = now.format('h:mm A');
+
+document.getElementById('current-day').textContent = currentDay;
+document.getElementById('current-time').textContent = currentTime;
